2691bb07a93b4b2d88358a48f82965b1bd892ea6
andrevks <italiabk@gmail.com>
2021-03-16 00:57:49 -0400

    build: setting up select, where, orderby

--- weekBeforeEvent/src/fluentSQL.js
+++ weekBeforeEvent/src/fluentSQL.js
@@ -1,11 +1,40 @@
 export default class FluentSQLBuilder {
   #database = [] //private member
+  #limit = 0
+  #select = []
+  #where = []
+  #orderBy = ''
+
   constructor({ database }){
     this.#database = database
   }
 
   static for(database){
     return new FluentSQLBuilder({ database })
+  }
+
+  limit(max){
+    this.limit = max
+    //Return the context to use build 
+    return this
+  }
+
+  select(props){
+    this.#select = props
+
+    return this
+  }
+
+  where(query){
+    this.#where = query
+
+    return this
+  }
+
+  orderBy(query){
+    this.#where = query
+
+    return this
   }
 
   build(){
